cc1b0d8a71e973821ea009cf0cbccf9dfdd56717
Kritu1418 <jhakritika957@gmail.com>
2025-12-09 22:30:46 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,10 +8,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// API Routes
+// IMPORT ROUTE FOR RENDER IMPORT
+app.use("/api", require("./routes/import.routes"));
+
+// SALES ROUTES
 app.use('/api', saleRoutes);
 
-// Server Start
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
