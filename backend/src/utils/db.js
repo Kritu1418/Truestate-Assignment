2cb8b590e855a394d64c9edb951ffc66c91ed719
Kritu1418 <jhakritika957@gmail.com>
2025-12-09 23:00:10 +0530

    Update db.js

--- backend/src/utils/db.js
+++ backend/src/utils/db.js
@@ -1,18 +1,9 @@
 const path = require("path");
-const fs = require("fs");
 const Database = require("better-sqlite3");
 
-// Render or local par database folder create karo
-const dbDir = path.join(__dirname, "../../database");
+// Render absolute path fix
+const dbPath = path.join(__dirname, "../database/sales.db");
 
-if (!fs.existsSync(dbDir)) {
-  fs.mkdirSync(dbDir, { recursive: true });
-}
-
-// DB file path
-const dbPath = path.join(dbDir, "sales.db");
-
-// Connect
 const db = new Database(dbPath);
 
 module.exports = db;
