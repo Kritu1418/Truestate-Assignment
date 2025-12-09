fa05ee992b6cf36634a11e15eb57c715cb2afad7
Kritu1418 <jhakritika957@gmail.com>
2025-12-09 23:09:06 +0530

    Update db.js

--- backend/src/utils/db.js
+++ backend/src/utils/db.js
@@ -1,9 +1,7 @@
 const path = require("path");
 const Database = require("better-sqlite3");
 
-// Render absolute path fix
 const dbPath = path.join(__dirname, "../database/sales.db");
-
 const db = new Database(dbPath);
 
 module.exports = db;
